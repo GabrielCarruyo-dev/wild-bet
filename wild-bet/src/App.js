b3cd0f59326dfc0db75dcf3600d46931973b1780
GabrielCarruyo-dev <gabrielcarruyo.dev@gmail.com>
2024-07-23 00:44:39 +0000

    yeahgit status

--- wild-bet/src/App.js
+++ wild-bet/src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
-import React, { useState } from 'react';
+import { BeakerIcon } from '@heroicons/react/24/solid'
+import React from 'react';
 import HeaderLong from './components/header';
+import Animals from './components/animals-ratio';
 function app() {
-
-  const [show, setshow] = useState(false);
   return (
       <div>
         <HeaderLong />
+        <BeakerIcon className="size-6 text-blue-500" />
+        <Animals />
       </div>
   );
 }
 export default app;
+
